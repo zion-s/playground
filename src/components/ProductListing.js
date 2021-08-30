6efb7d9515bd00e2bb25911282c30dabdea0162a
Zion Sandeep <zion.sandeep519@gmail.com>
2021-08-30 23:44:11 +0530

    feat: Added redux boilerplate and app component tree

---
+++ src/components/ProductListing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
+
+const ProductListing = () => {
+  const products = useSelector((state) => state);
+  console.log(products);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
+
+export default ProductListing;
